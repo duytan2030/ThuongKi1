6bbea5662ebde47bf9d7bb4e4f355e1608215c70
duytan2030 <34102079+duytan2030@users.noreply.github.com>
2019-09-09 22:13:45 +0700

    cap nhat moi nhat

--- app/src/main/java/com/example/ndt_cau1/MainActivity.java
+++ app/src/main/java/com/example/ndt_cau1/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.ndt_cau1;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,11 +13,13 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     CheckBox chk1;
-    Button btn_dangnhap;
+    Button btn_dangnhap,btn_thoat;
+    private Dialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         chk1 =(CheckBox) findViewById(R.id.checkbox_chk1);
         btn_dangnhap =(Button) findViewById(R.id.button_dangnhap);
         btn_dangnhap.setOnClickListener(new View.OnClickListener() {
@@ -28,5 +33,39 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        btn_thoat =(Button) findViewById(R.id.button_thoat);
+        btn_thoat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showAlertDialog();
+            }
+        });
+    }
+//    public void showDialog(){
+//        dialog = new Dialog(MainActivity.this);
+//        dialog.setTitle("Thangcode.com");
+//        dialog.setContentView(R.layout.dialog);
+//        dialog.show();
+//    }
+    public void showAlertDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Thong Bao");
+        builder.setMessage("Ban co muon thoat ra khong?");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Khong", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this, "Không thoát được", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("Co", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
     }
 }
